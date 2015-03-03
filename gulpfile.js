b3bf1c20e5a44e26589942326e0511ebe7a42f1b
Hjalti Leifsson <Hjaltileifsson@gmail.com>
2015-03-03 14:13:21 +0000

    Added directory structure and dependencies.

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify'),
+    jshint = require('gulp-jshint'),
+    concat = require('gulp-concat'),
+    ngAnnotate = require('gulp-ng-annotate'),
+    stylish = require('jshint-stylish');
+
+var paths = {
+	scripts: ['apps/**/*.js']
+};
+
+gulp.task('default', function() {
+	
+});
