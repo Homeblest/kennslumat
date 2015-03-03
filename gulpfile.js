98df9a7c2c9791633a88e9a9d1139888f1195672
Hjalti Leifsson <Hjaltileifsson@gmail.com>
2015-03-03 14:42:38 +0000

    Configured gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -10,5 +10,32 @@ var paths = {
 };
 
 gulp.task('default', function() {
-	
+	return gulp.src(paths.scripts)
+		.pipe(jshint({
+            curly: true,
+            immed: true,
+            newcap: true,
+            noarg: true,
+            sub: true,
+            boss: true,
+            eqnull: true,
+            node: true,
+            undef: true,
+            globals: {
+                _: false,
+                jQuery: false,
+                angular: false,
+                moment: false,
+                console: false,
+                $: false,
+                RuChat: true,
+                io: true
+            }
+        }))
+        .pipe(jshint.reporter(stylish))
+        .pipe(jshint.reporter('fail'))
+        .pipe(ngAnnotate())
+        .pipe(uglify())
+        .pipe(concat('kennslumat.min.js'))
+        .pipe(gulp.dest('app/js'))
 });
