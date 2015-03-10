d7c86a81fec801d44d6d922ccaaca3ac6976ab1b
Hjalti Leifsson <Hjaltileifsson@gmail.com>
2015-03-10 13:43:32 +0000

    Added code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -33,14 +33,24 @@ module.exports = function(config) {
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
+        preprocessors: {
+            // source files, that you wanna generate coverage for
+            // do not include tests or libraries
+            'app/finalConcat.js': ['coverage']
+        },
+
+        // optionally, configure the reporter
+        coverageReporter: {
+            type: 'html',
+            dir: 'coverage/'
+        },
 
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress'/*, 'html'*/],
-/*
+        reporters: ['progress', 'coverage' /*, 'html'*/ ],
+        /*
         htmlReporter: {
             outputFile: 'app/test/units.html'
         },
