976b9842bd142ead90a085a2acecf3aed6e60e8a
Árni Benedikt Árnason <arnibarnason@gmail.com>
2015-03-15 14:04:59 +0000

    tests in progress

---
+++ tests/adminDashboardController.spec.js
@@ -0,0 +1,52 @@
+describe('adminDashboardController', function() {
+    beforeEach(module('evalApp'));
+/*
+    var scope,
+        rootScope,
+        fakeFactory,
+        controller,
+        q,
+        deferred,
+        httpBackend;
+
+    beforeEach(function() {
+        fakeFactory = {
+            login: function(loginData) {
+
+                deferred = q.defer();
+
+                // Place the fake return object here
+                deferred.resolve();
+
+                return deferred.promise;
+            },
+            getCourses: function() {
+                deferred = q.defer();
+
+                // Place the fake return object here
+                deferred.resolve();
+
+                return deferred.promise;
+            }
+        };
+        spyOn(fakeFactory, 'getCourses').and.callThrough();
+    });
+
+    //Inject fake factory into controller
+    beforeEach(inject(function($rootScope, $controller, $q, mainFactory) {
+        rootScope = $rootScope;
+        scope = $rootScope.$new();
+        q = $q;
+        controller = $controller('adminDashboardController', {
+            $scope: scope,
+            mainFactory: fakeFactory,
+            $rootScope: rootScope,
+            $httpBackend: httpBackend
+        });
+    }));
+
+    it('', function() {
+        expect(fakeFactory.getCourses).toHaveBeenCalled();
+    })
+*/
+});
