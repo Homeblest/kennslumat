65f7c69796839f4025f7870ddba9a6ef009357ed
Hjalti Leifsson <Hjaltileifsson@gmail.com>
2015-03-18 01:05:58 +0000

    Revert directive changes

--- app/controllers/evaluationController.js
+++ app/controllers/evaluationController.js
@@ -8,23 +8,8 @@ evalApp.controller('evaluationController', function($scope, $rootScope, $http, $
 
     $state.go('evaluationView.IntroText');
 
-    $scope.courseAnswersArray = [];
-
     mainFactory.getEvaluationByCourse($stateParams.course, $stateParams.semester, $stateParams.evaluationID)
         .success(function(data) {
             $scope.evaluation = data;
-
-            if (typeof $scope.evaluation.CourseQuestions.length !== 0) {
-                for (var i = 0; i < $scope.evaluation.CourseQuestions.length; i++) {
-                    var newAnswer = new EvaluationAnswerDTO(i, null, "");
-                    $scope.courseAnswersArray.push(newAnswer);
-                }
-            }
-
         });
-
-    $scope.printArray = function() {
-        console.log($scope.courseAnswersArray);
-    };
-
 });
